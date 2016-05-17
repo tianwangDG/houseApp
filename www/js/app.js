750b8ebbf7b5dee8a98b999be22c5c19fca8d360
yaoyonstudio <yaoyonstudio@gmail.com>
2016-05-17 18:33:36 +0800

    '20160517'

--- www/js/app.js
+++ www/js/app.js
@@ -1,85 +1,110 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.constant('appInfo', {
+	apiUrl: 'http://app.tigonetwork.com/api',
+})
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider, $compileProvider) {
+//.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider, $compileProvider, ionicDatePickerProvider) {
+  $ionicConfigProvider.platform.ios.tabs.style('standard');
+  $ionicConfigProvider.platform.ios.tabs.position('bottom');
+  $ionicConfigProvider.platform.android.tabs.style('standard');
+  $ionicConfigProvider.platform.android.tabs.position('bottom');
+
+  $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+  $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+
+  $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+  $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+  $ionicConfigProvider.platform.ios.views.transition('ios');
+  $ionicConfigProvider.platform.android.views.transition('android');
+
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(geo|mailto|tel|maps):/)
+
+  //日期选择器
+  //var datePickerObj = {
+  //  inputDate: new Date(),
+  //  setLabel: '确定',
+  //  todayLabel: '今天',
+  //  closeLabel: '关闭',
+  //  mondayFirst: false,
+  //  weeksList: ["日", "一", "二", "三", "四", "五", "六"],
+  //  monthsList: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+  //  templateType: 'popup',
+  //  from: new Date(2016, 1, 1),
+  //  to: new Date(2030, 12, 31),
+  //  showTodayButton: true,
+  //  dateFormat: 'yyyy - MM - dd',
+  //  closeOnSelect: false,
+  //  disableWeekdays: [],
+  //};
+  //ionicDatePickerProvider.configDatePicker(datePickerObj);
+
+
   $stateProvider
 
-  // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
-  // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.index', {
+    url: '/index',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-index': {
+        templateUrl: 'templates/tab-index.html',
+        controller: 'indexCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.car', {
+      url: '/car',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-car': {
+          templateUrl: 'templates/tab-car.html',
+          controller: 'carCtrl'
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.concern', {
+    url: '/concern',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-concern': {
+        templateUrl: 'templates/tab-concern.html',
+        controller: 'concernCtrl'
       }
     }
+  })
+
+  .state('tab.message', {
+	  url: '/message',
+	  views: {
+		  'tab-message': {
+			  templateUrl: 'templates/tab-message.html',
+			  controller: 'messageCtrl'
+		  }
+	  }
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/index');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,71 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('indexCtrl', function($scope, $rootScope, $state, $ionicSlideBoxDelegate,$http, appInfo, Slider, House) {
+	//显示滑动图片
+	Slider.get().$promise.then(function(response){
+		$scope.sliders = response.data;
+		$ionicSlideBoxDelegate.update();
+	})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+	//筛选数据
+
+
+	$scope.showLpInit = {
+		hasMore: true,
+		page: 1
+	};
+	$scope.result = [];
+
+
+	//楼盘数据
+	House.get({page:$scope.showLpInit.page}).$promise.then(function(response){
+		$scope.fcData = response.data;
+		//console.log($scope.fcData);
+		$scope.result = $scope.fcData;
+		console.log($scope.result);
+	});
+
+	$scope.loadMore = function(){
+		//console.log('dd');
+		$scope.showLpInit.page++;
+		House.get({page:$scope.showLpInit.page}).$promise.then(function(response){
+			console.log($scope.showLpInit.page);
+			console.log(response.data);
+			if(response.data.length>0){
+				for(var i=0;i<response.data.length;i++){
+					$scope.result.push(response.data[i]);
+				}
+			}
+			if(response.data.length<10){
+				$scope.showLpInit.hasMore = false;
+			}
+
+		});
+		$scope.$broadcast('scroll.infiniteScrollComplete');
+	}
+
+
+
+
+	//设置特色标签颜色
+	$scope.colors = ["#f3541f","#326ed7","#653a78","#34ab55","#ffbc44","#039BE5","#009688","#536DFE","#AB47BC","#E53935","#3F51B5"];
+
+
+
+
+
+
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('carCtrl', function($scope) {
+
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('concernCtrl', function($scope) {
+
+})
+
+.controller('messageCtrl', function($scope) {
+
 });
